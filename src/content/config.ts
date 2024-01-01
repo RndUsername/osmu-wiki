a445e7ab02e7b890ee9d2b644bf52913d1a25451
Kevin Koch <k.koch@notabot.cc>
2024-01-01 19:33:57 +0100

    display logo of project and some fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z, type SchemaContext } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
-export const collections = {
-	docs: defineCollection({ schema: docsSchema({
+function schema(context: SchemaContext){
+	const docSch = docsSchema({
 		extend: z.object({
 			githubRepo: z.string().optional(),
 			discordChannel: z.string().optional(),
-			shops: z.array(z.object({
-				name: z.string(),
-				link: z.string(),
-				official: z.boolean().optional(),
-				region: z.enum([
-					"US", "EU", "CA"
-				]).optional()
-			})).optional()
+			shops: z.array(
+				z.object({
+					name: z.string(),
+					link: z.string(),
+					official: z.boolean().optional(),
+					region: z.string().optional()
+				})
+			).optional(),
+			logo: context.image().optional()
 		}),
-	  }) 
-	}),
+	})
+
+	return docSch(context)
+}
+
+export const collections = {
+	docs: defineCollection({ schema }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
