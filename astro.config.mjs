f470dfc22d7fca7e118bee4c640cc539dc757b70
WantClue <Lars_Naether@hotmail.de>
2024-01-30 10:07:26 +0100

    add qaxe

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,111 +6,164 @@ import links from "./links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "Wiki",
-    logo: {
-      src: "./src/assets/osmu-logo.svg"
-    },
-    social: {
-      discord: links.discordInvite
-    },
-    editLink: {
-      baseUrl: links.gitEditBase
-    },
-    sidebar: [{
-      label: "OSMU",
-      items: [{
-        label: "About",
-        link: "/osmu/about"
-      }]
-    }, {
-      label: "Bitaxe",
-      items: [{
-        label: "Overview",
-        link: "/bitaxe/about"
-      }, {
-        label: "100 / Max",
-        link: "/bitaxe/100"
-      }, {
-        label: "200 / Ultra",
-        link: "/bitaxe/200"
-      }, {
-        label: "300 / Hex",
-        link: "/bitaxe/300"
-      }]
-    }, {
-      label: "Nerdminer",
-      items: [{
-        label: "Overview",
-        link: "/nerdminer/about"
-      }]
-    }, {
-      label: "Public Pool",
-      items: [{
-        label: "Overview",
-        link: "/public-pool/about"
-      }]
-    }, {
-      label: "AxeOS",
-      items: [{
-        label: "Overview",
-        link: "/axeos/about"
-      },{
-        label: "Install onto BitAxe",
-        link: "/axeos/install-onto-bitaxe"
-      },{
-        label: "Build from Source",
-        link: "/axeos/compile"
-      }]
-    }, {
-      label: "Bitcrane",
-      items: [{
-        label: "Overview",
-        link: "/bitcrane/about"
-      }]
-    }, {
-      label: "Piaxe",
-      items: [{
-        label: "Overview",
-        link: "/piaxe/about"
-      }, {
-        label: "Assemble Piaxe",
-        link: "/piaxe/assembly"
-      }, {
-        label: "Building Piaxe",
-        link: "/piaxe/building"
-      }]
-    }, {
-      label: "Antsniffer",
-      items: [{
-        label: "Overview",
-        link: "/antsniffer/about"
-      }]
-    }, {
-      label: "OSMU Lab",
-      items: [{
-        label: "Overview",
-        link: "/osmu-lab/about"
-      }, {
-        label: "BM1397",
-        link: "/osmu-lab/bm1397"
-      }]
-    },{
-      label: "Tips & Tricks",
-      items: [{
-        label: "Building PCBs",
-        link: "/tips/building-pcbs"
-      }, {
-        label: "Assembly",
-        link: "/tips/assembly"
-      }]
-    }],
-    customCss: ['./src/tailwind.css'],
-    components: {
-      PageTitle: "./src/components/PageTitle.astro"
-    }
-  }), tailwind({
-    // Disable the default base styles:
-    applyBaseStyles: false
-  }), svelte()],
+  integrations: [
+    starlight({
+      title: "Wiki",
+      logo: {
+        src: "./src/assets/osmu-logo.svg",
+      },
+      social: {
+        discord: links.discordInvite,
+      },
+      editLink: {
+        baseUrl: links.gitEditBase,
+      },
+      sidebar: [
+        {
+          label: "OSMU",
+          items: [
+            {
+              label: "About",
+              link: "/osmu/about",
+            },
+          ],
+        },
+        {
+          label: "Bitaxe",
+          items: [
+            {
+              label: "Overview",
+              link: "/bitaxe/about",
+            },
+            {
+              label: "100 / Max",
+              link: "/bitaxe/100",
+            },
+            {
+              label: "200 / Ultra",
+              link: "/bitaxe/200",
+            },
+            {
+              label: "300 / Hex",
+              link: "/bitaxe/300",
+            },
+          ],
+        },
+        {
+          label: "Nerdminer",
+          items: [
+            {
+              label: "Overview",
+              link: "/nerdminer/about",
+            },
+          ],
+        },
+        {
+          label: "Public Pool",
+          items: [
+            {
+              label: "Overview",
+              link: "/public-pool/about",
+            },
+          ],
+        },
+        {
+          label: "AxeOS",
+          items: [
+            {
+              label: "Overview",
+              link: "/axeos/about",
+            },
+            {
+              label: "Install onto BitAxe",
+              link: "/axeos/install-onto-bitaxe",
+            },
+            {
+              label: "Build from Source",
+              link: "/axeos/compile",
+            },
+          ],
+        },
+        {
+          label: "Bitcrane",
+          items: [
+            {
+              label: "Overview",
+              link: "/bitcrane/about",
+            },
+          ],
+        },
+        {
+          label: "Piaxe",
+          items: [
+            {
+              label: "Overview",
+              link: "/piaxe/about",
+            },
+            {
+              label: "Assemble Piaxe",
+              link: "/piaxe/assembly",
+            },
+            {
+              label: "Building Piaxe",
+              link: "/piaxe/building",
+            },
+          ],
+        },
+        {
+          label: "Qaxe",
+          items: [
+            {
+              label: "Overview",
+              link: "/qaxe/about",
+            },
+          ],
+        },
+        {
+          label: "Antsniffer",
+          items: [
+            {
+              label: "Overview",
+              link: "/antsniffer/about",
+            },
+          ],
+        },
+        {
+          label: "OSMU Lab",
+          items: [
+            {
+              label: "Overview",
+              link: "/osmu-lab/about",
+            },
+            {
+              label: "BM1397",
+              link: "/osmu-lab/bm1397",
+            },
+          ],
+        },
+        {
+          label: "Tips & Tricks",
+          items: [
+            {
+              label: "Building PCBs",
+              link: "/tips/building-pcbs",
+            },
+            {
+              label: "Assembly",
+              link: "/tips/assembly",
+            },
+          ],
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+      components: {
+        PageTitle: "./src/components/PageTitle.astro",
+      },
+    }),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+    svelte(),
+  ],
 });
---
+++ src/content/docs/qaxe/about.md
@@ -0,0 +1,16 @@
+---
+title: About Qaxe
+logo: /src/assets/osmu-logo.svg
+discordChannel: https://discord.com/channels/1091348375301013615/1167074832186355752
+githubRepo: https://github.com/shufps/qaxe
+---
+
+Qaxe is a quad-BM1366 Miner based on the [PiAxe](https://github.com/shufps/piaxe) and [BitAxe](https://github.com/skot/bitaxe/tree/ultra-v1.3).
+
+**rev1:** is tested and operating at about 1.8TH/s average speed.<br>
+**rev2:** working fine with the expected speed of ~1.8TH/s avg after some minor modifications (330µF caps are wrongly placed, see rev3)<br>
+**rev3:** Fixed Caps placement and added Boot-Switch. It should put the STM32 into DFU bootloader but not tested yet.
+
+:::caution[This page is not finally written yet.]
+Help us to complete the wiki by using the "Edit page" button at the end of the page 👇
+:::
