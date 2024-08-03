939ffc51d6ba4eac9b50653649623fa9ee1ac261
WantClue <Lars_Naether@hotmail.de>
2024-08-03 12:46:07 +0200

    adding gamma

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,6 +52,10 @@ export default defineConfig({
               link: "/bitaxe/400",
             },
             {
+              label: "600 / Gamma",
+              link: "/bitaxe/600",
+            },
+            {
               label: "API",
               link: "/bitaxe/api",
             },
